88ead641e838d5d66d122b0de3b6987a95612a27
ThanhVy212 <buivy22122004@gmail.com>
2025-11-05 23:01:52 +0700

    feat: add user management api

--- backend/authentication_KYC/src/routes/adminRoute.js
+++ backend/authentication_KYC/src/routes/adminRoute.js
@@ -1,15 +1,21 @@
 import express from "express";
 import { authorize, protectedRoute } from "../middlewares/authMiddleware.js";
-import { createAccount } from "../controllers/adminController.js";
+import {
+  createAccount,
+  getAllRoles,
+  getAllUsers,
+  getUser,
+  updateUserRole,
+} from "../controllers/adminController.js";
 
 const router = express.Router();
 
 router.use(protectedRoute, authorize("admin"));
 
 router.post("/create-account", createAccount);
-// router.get('/users', getAllUsers);
-// router.get('/users/:id', getUser)
-// router.patch('/users/:id/role', updateUserRole)
-// router.get('/roles', getAllRoles)
+router.get("/users", getAllUsers);
+router.get("/users/:id", getUser);
+router.patch("/users/:id/role", updateUserRole);
+router.get("/roles", getAllRoles);
 
 export default router;
